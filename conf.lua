6e7090a6e41ea7af94e8ff0b4cd5e46881322a4f
Cynthia Allen <cynthiaallen228@gmail.com>
2025-03-03 16:34:54 -0800

    add conf.lua

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+	t.window.width = 1280
+	t.window.height = 720
+
+	t.modules.joystick = false
+	t.modules.physics = false
+	t.modules.data = false
+
+	t.window.title = "Checkers"
+end
